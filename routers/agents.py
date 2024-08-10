9bcf46cfe4f5a1cb8905624369c1df7626b7fea5
Ryan Luong <ryanluong83@gmail.com>
2024-08-09 17:55:40 -0700

    Changed the get_all_agents to get all the valid agents

--- routers/agents.py
+++ routers/agents.py
@@ -3,23 +3,23 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from schemas.common_schemas import AgentBase
-from models.common_models import Agent
+from models.common_models import Agents
 from utility.db import get_db
 
 router = APIRouter()
 
 @router.get("/agents", response_model=AgentBase)
 async def get_all_agents(db: Session = Depends(get_db)):
-    agents = jsonable_encoder(db.query(Agent).all())
-    response = {item["agent"]: item["agent_id"] for item in agents}
+    agents = jsonable_encoder(db.query(Agents).all())
+    response = {item["agent"]: item["agent_id"] for item in agents if item["agent"]}
     return JSONResponse(content=response)
 
 @router.get("/agents/{identifier}", response_model=AgentBase)
 async def get_agent(identifier: str, db: Session = Depends(get_db)):
     if identifier.isdigit():
-        agent = db.query(Agent).filter(Agent.agent_id == int(identifier)).first()
+        agent = db.query(Agents).filter(Agents.agent_id == int(identifier)).first()
     else:
-        agent = db.query(Agent).filter(Agent.agent == identifier).first()
+        agent = db.query(Agents).filter(Agents.agent == identifier).first()
     
     if not agent:
         raise HTTPException(status_code=404, detail=f"The agent was not found based on the id/name: {identifier}")
